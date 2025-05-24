988e7c5e2e6bfd5654f57c7a410e4018da9debe4
hafsakose <kose.hafsa.0@gmail.com>
2025-05-24 23:56:49 +0300

    login php kismi

--- login.php
+++ login.php
@@ -1,1 +1,24 @@
-  
+<?php
+$dogru_kullanici = "2023123456"; 
+$dogru_sifre = "1234";
+
+
+$kullanici = $_POST['username'];
+$sifre = $_POST['password'];
+
+
+if (filter_var($kullanici, FILTER_VALIDATE_EMAIL)) {
+    /
+    header("Location: login.html");
+    exit();
+}
+
+
+if ($kullanici === $dogru_kullanici && $sifre === $dogru_sifre) {
+    echo "<h2>Hoş geldiniz, $kullanici </h2>";
+} else {
+   
+    header("Location: login.html");
+    exit();
+}
+?>
